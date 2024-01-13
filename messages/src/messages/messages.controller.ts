5bc517df7f9b6ded40a1e299404a2c36d97a2f56
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2024-01-13 12:33:51 +0100

    Messages - update controller

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,18 +1,28 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { MessagesService } from './messages.service';
 import { StoreDto } from './dtos/store.dto';
 
 @Controller('messages')
 export class MessagesController {
+  private readonly messageService: MessagesService;
+
+  constructor() {
+    // Nest uses dependency injection, don't do this in real apps
+    this.messageService = new MessagesService();
+  }
+
   @Get()
-  list() {}
+  list() {
+    return this.messageService.findAll();
+  }
 
   @Get('/:id')
   get(@Param('id') id: string) {
-    return id;
+    return this.messageService.findOne(id);
   }
 
   @Post()
   store(@Body() body: StoreDto) {
-    return JSON.stringify(body);
+    return this.messageService.create(body.content);
   }
 }
